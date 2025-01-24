d20f7ac56d0b0f86dfad075286169e522d64d1b3
Tiara Rojas <tiara.rojas@niceincontact.dom>
2025-01-24 09:12:31 -0400

    Snake tail added

---
+++ My project/Assets/Tail.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Tail : MonoBehaviour
+{
+    public float pointSpacing = .1f;
+    List<Vector2> points;
+    LineRenderer line;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+
+        points = new List<Vector2>();
+
+        SetPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(points.Last(), transform.position) > pointSpacing) 
+            SetPoint();
+    }
+
+    void SetPoint()
+    {
+        points.Add(transform.position);
+        line.positionCount = points.Count;
+        line.SetPosition(points.Count - 1, transform.position);
+    }
+}
