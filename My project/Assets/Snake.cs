73cc0c65dfb48239e33bc0d563673560fa129365
Tiara Rojas <tiara.rojas@niceincontact.dom>
2025-01-24 12:10:01 -0400

    snake movement added

---
+++ My project/Assets/Snake.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    public float speed = 3f;
+    public float rotationSpeed = 200f;
+
+    float horizontal = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal= Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.up*speed*Time.fixedDeltaTime, Space.Self);
+        transform.Rotate(Vector3.forward * -horizontal* rotationSpeed*Time.fixedDeltaTime);
+    }
+}
