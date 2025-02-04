bfbd11f828cb490bfce7cf9201d4c8da2aa016cb
Tiara Rojas <tiara.rojas@niceincontact.dom>
2025-02-04 12:28:21 -0400

    added player 2

--- My project/Assets/Snake.cs
+++ My project/Assets/Snake.cs
@@ -5,16 +5,23 @@ public class Snake : MonoBehaviour
     public float speed = 3f;
     public float rotationSpeed = 200f;
 
+    public string inputAxis = "Horizontal";
+
     float horizontal = 0f;
     // Update is called once per frame
     void Update()
     {
-        horizontal= Input.GetAxisRaw("Horizontal");
+        horizontal= Input.GetAxisRaw(inputAxis);
     }
 
     private void FixedUpdate()
     {
         transform.Translate(Vector2.up*speed*Time.fixedDeltaTime, Space.Self);
         transform.Rotate(Vector3.forward * -horizontal* rotationSpeed*Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
     }
 }
--- My project/Assets/Tail.cs
+++ My project/Assets/Tail.cs
@@ -3,17 +3,19 @@ using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(EdgeCollider2D))]
 public class Tail : MonoBehaviour
 {
     public float pointSpacing = .1f;
     List<Vector2> points;
     LineRenderer line;
+    EdgeCollider2D col;
     public Transform snake;
     // Start is called before the first frame update
     void Start()
     {
         line = GetComponent<LineRenderer>();
-
+        col = GetComponent<EdgeCollider2D>();
         points = new List<Vector2>();
 
         SetPoint();
@@ -28,8 +30,13 @@ public class Tail : MonoBehaviour
 
     void SetPoint()
     {
+        if (points.Count > 1)
+            col.points = points.ToArray<Vector2>();
+
         points.Add(snake.position);
         line.positionCount = points.Count;
         line.SetPosition(points.Count - 1, snake.position);
+
+        
     }
 }
